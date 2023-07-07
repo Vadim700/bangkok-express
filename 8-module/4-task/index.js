79cbf99eb9b58f96bec1d88b7be0640054256a64
Vadim <bekmansurov700@mail.ru>
2023-07-07 11:53:52 +0500

    Update index.js

--- 8-module/4-task/index.js
+++ 8-module/4-task/index.js
@@ -192,7 +192,7 @@ export default class Cart {
             <p>
               Order successful! Your order is being cooked :) <br>
               We’ll notify you about delivery time shortly.<br>
-              <img src="/assets/images/delivery.gif">
+              <img src="./assets/images/delivery.gif">
             </p>
           </div>
         `;
