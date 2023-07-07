7d90a5fef7f6544a51764d9776e151d68f4424fe
Vadim <bekmansurov700@mail.ru>
2023-07-07 10:32:37 +0500

    Update index.js

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -27,7 +27,7 @@ export default class Modal {
         <div class="modal__inner">
           <div class="modal__header">
             <button type="button" class="modal__close">
-              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+              <img src="./assets/images/icons/cross-icon.svg" alt="close-icon" />
             </button>
             <h3 class="modal__title">Вот сюда нужно добавлять заголовок</h3>
           </div>
